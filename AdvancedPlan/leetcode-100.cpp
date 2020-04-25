8a86352e8dc38c8972277ac1ca0a38a05e4466dd
askr <m3933030@gmail.com>
2020-04-25 19:27:00 +0800

    Update leetcode-100.cpp

--- AdvancedPlan/leetcode-100.cpp
+++ AdvancedPlan/leetcode-100.cpp
@@ -1,18 +1,28 @@
-#include<iostream>
-#include<cmath>
 #include<vector>
-#include<string>
+#include<iostream>
+#include<algorithm>
+#include<queue>
 using namespace std;
+
 struct TreeNode {
     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 bool isSameTree(TreeNode* p, TreeNode* q) {
-    if(p->val!=q->val)return false;
-    if ()
-    if(p->left && q->left) return isSameTree(p->left,q->left);
-    if(p->right && q->right) return isSameTree(p->right,q->right);
+    //循环不变式: 当前节点
+    if (!p && !q) return true;
+    else if (!p && q) return false;
+    else if (p && !q) return false;
+    else if (!p->left && q->left) return false;
+    else if (p->left && !q->left) return false;
+    else if (!p->right && q->right) return false;
+    else if (p->right && !q->right) return false;
+    else if (p->val != q->val) return false;
+    else if (!isSameTree(p->left,q->left) || !isSameTree(p->right,q->right)) return false;
     return true;
+}
+int main(void){
+
 }
