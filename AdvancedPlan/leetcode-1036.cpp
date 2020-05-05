fd7979e6ceffc1963c7f027d61d5a688ab6d04aa
askr <m3933030@gmail.com>
2020-05-05 22:50:32 +0800

    add

---
+++ AdvancedPlan/leetcode-1036.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isEscapePossible(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
+
+    }
+};
+
+int main(void){
+    Solution temp{};
+    vector<vector<int>> input_data{};
+    vector<int> source{};
+    vector<int> target{}; 
+    cout<<temp.isEscapePossible(input_data,source,target);
+}
