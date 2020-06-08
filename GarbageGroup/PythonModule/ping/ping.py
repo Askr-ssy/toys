ca45f175a3c2ff4f0c144fb2f9315fdeefb21314
askr <m3933030@gmail.com>
2020-06-09 00:16:08 +0800

    add

---
+++ GarbageGroup/PythonModule/ping/ping.py
@@ -0,0 +1,5 @@
+def ping():
+    return "ping"
+
+if __name__=="__main__":
+    print("this is a ping main")
---
+++ GarbageGroup/PythonModule/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+
+
+
+
+
+
+
+
+
+
+
+setup(
+    name="ping",
+    version="1.0.2",
+    description='This is a test of the setup',
+    author='askr',
+    package=["ping"]
+)
