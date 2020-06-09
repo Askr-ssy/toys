b371afec001c07fb96e01f3c64d881e4e37a7fa7
askr <askr>
2020-06-09 18:37:16 +0800

    add

---
+++ CPlusPlusThings/examples/const_example.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+#include<iostream>
+using namespace std;
+int main(){
+    const int num=0;
+    const int * const ptr=&num; //error! const int* -> int*
+    cout<<*ptr<<endl;
+}
