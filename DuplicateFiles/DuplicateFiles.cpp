a7d79ebc747a49198f9fd78568b0b5691c5da7d1
askr <m3933030@gmail.com>
2020-06-18 18:23:07 +0800

    add

--- DuplicateFiles/DuplicateFiles.cpp
+++ DuplicateFiles/DuplicateFiles.cpp
@@ -6,42 +6,44 @@
 #include<iostream>
 #include<vector>
 #include<map>
-#include"openssl/md5.h"
+#include<iostream>
 namespace fs = std::filesystem;
 using namespace std;
-vector<fs::path> find_file_on_dire(const fs::path& dire,vector<fs::path> &allfile) {
-	for (const auto& entry : fs::directory_iterator(dire)) {
-		allfile.push_back(entry.path());
-		try {
-			if (fs::is_directory(entry))
-				find_file_on_dire(entry,allfile);
-		}
-		catch (exception err) {
-			cerr << entry.path() << " ";
-			cerr << err.what() << endl;
-			continue;
-		}
-	}
-	return allfile;
-}
 
-int main()
+std::vector<fs::path> find_all_file(const fs::path &path,vector<fs::path> &allfile){
+    for (auto &p: fs::recursive_directory_iterator(path)){
+        // cout<<p.path()<<endl;
+        allfile.push_back(p.path());
+    }
+    return allfile;
+}
+int main(int argc,char **argv)
 {
-	std::string path = "D:";
-	vector<fs::path> allfile{};
+    if (argc<2){
+        cerr<<"path not found"<<endl;
+        exit(1);
+    }
+	std::string path{argv[1]};
+
+    vector<fs::path> allfile{};
 	long long sum_filesize{};
 
-	if (! std::filesystem::exists(path)) {
-		cerr << "path error" << endl;
+	if (! fs::exists(path)) {
+		cerr << "path error:not found" << endl;
 		exit(1);
 	}
+    if (! fs::is_directory(path)){
+        cerr<<"path is a file"<<endl;
+        exit(1);
+    }
 
 	try {
-		find_file_on_dire(path,allfile);
+		find_all_file(path,allfile);
 		cout << "查找完成,共:" << allfile.size() << "个文件 " << endl;;
 	}
 	catch (exception &err){
 		cerr << err.what() << endl;
 	}
- 	return 0;
+ 	return 0;   
+    
 }
