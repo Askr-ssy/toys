f1f8a6358bd08d89dad2f5904cdc07ccb9a7da10
askr-ssy <Askr-ssy@users.noreply.github.com>
2021-02-20 00:05:03 +0800

    add

--- IPC/Pi/Pi.cpp
+++ IPC/Pi/Pi.cpp
@@ -1,77 +1,31 @@
+#include <stdio.h>
+
 #include <iostream>
-#include <cstdio>
-void CaculatePi(int len)
-{
-	int i;    
-	int numberator = 1,denominator = 3,result,carry;
-	int flag = 1,count = 0;      //继续循环的标志及循环的次数
-	char *pi,*temp;              //指向保存pi值和临时计算结果的数据
-
-    std::cout << len<<std::endl;
-	len += 2;   //增加两位
-	if(!(pi = (char*)malloc(sizeof(char)*len)))   //分配保存pi值的内存
-	{
-		std::cout<<"分配内存失败!" <<std::endl;
-		exit(0);
-	}
-	if(!(temp = (char*)malloc(sizeof(char)*len))) //分配保存呢临时值的内存
-	{
-		std::cout<< "分配内存失败！\n";
-		exit(0);
-	}
-
-	for(i = 0; i < len; i++)  //初始化数组
-	{
-		pi[i] = temp[i] = 0;
-	}
-	pi[1] = 2;         //置初值
-	temp[1] = 2; 
-
-	while(flag && (++count < 2147483647))  //int的最大值 2147483647
-	{
-		carry = 0;
-		for(i = len-1; i > 0; i--)     //从低位到高位相乘
-		{
-			result = temp[i] * numberator+carry; //用每一位去乘，再加上进位
-			temp[i] = result % 10;               //保存个数
-			carry = result / 10;                 //进位
-		}
-
-		carry = 0;
-		for(i = 0; i < len; i++)                 //有高位到低位进行除法运算
-		{
-			result = temp[i] + carry*10;         //当前位加上前一位的余数
-			temp[i] = result / denominator;      //当前位的整数部分
-			carry = result % denominator;        //当前位的余数，累加到下一位的运算
-		}
-		flag = 0; //清除标志
-
-		for(i = len-1; i > 0; i--)               
-		{   
-			result = pi[i] + temp[i];            //将计算结果累加到result中
-			pi[i] = result % 10;                 //保留一位
-			pi[i-1] += result / 10;              //向高位进位
-			flag |= temp[i];                     //若temp中的数全为0,退出循环
-		} 
-		numberator++;       //累加分子
-		denominator += 2;   //累加分母
-	}
-	std::cout << "计算了" << count << "次"<<std::endl;
-	std::cout << "PI = \n";
-	std::cout << (int)pi[1];
-	for(i = 2; i < len; i++)
-	{
-		if((i>2) && (i-2)%10 == 0)        //每10位小数间加一个空格
-			std::cout << " ";
-		if((i>2) && (i-2)%50 == 0)       //每50位小数换行
-			std::cout <<std::endl;
-
-		std::cout << (int)pi[i];         //输出一位小数
-	}
-	std::cout << std::endl;
-}
-int main(int argc,char ** argv)
-{
-    CaculatePi(std::atoi(argv[1]));
-    return 0;
+using namespace std;
+int main(
+    int argc,
+    char **
+        argv) { //本程序每四位数输出，如果请求计算的位数不是4的整数倍，最后输出可能会少1~3位数
+  long a[2] = {956, 80}, b[2] = {57121, 25}, i = 0, j, k, p, q, r, s = 2, t, u,
+       v, N, M = 10000;
+  N = std::atol(argv[1]);
+  N = N / 4 + 3;
+  long *pi = new long[N], *e = new long[N];
+  while (i < N)
+    pi[i++] = 0;
+  while (--s + 1) {
+    for (*e = a[k = s], i = N; --i;)
+      e[i] = 0;
+    for (q = 1; j = i - 1, i < N; e[i] ? 0 : ++i, q += 2, k = !k)
+      for (r = v = 0; ++j < N; pi[j] += k ? u : -u)
+        u = (t = v * M + (e[j] = (p = r * M + e[j]) / b[s])) / q, r = p % b[s],
+        v = t % q;
+  }
+  while (--i)
+    (pi[i] = (t = pi[i] + s) % M) < 0 ? pi[i] += M, s = t / M - 1 : s = t / M;
+//   for (cout << "3."; ++i < N - 2;)
+//     printf("%04ld", pi[i]);
+  delete[] pi;
+  delete[] e;
+  return 0;
 }
